9338a801bf1e573708ab15be12a3abdad9d9fc0f
Ubuntu <ubuntu@ip-172-31-1-155.ap-southeast-1.compute.internal>
2020-06-14 08:39:26 +0000

    initial push for lightgbm container

---
+++ light_gbm_sagemaker/script/build_and_push.sh
@@ -0,0 +1,17 @@
+ACCOUNT_ID=$1
+REGION=$2
+REPO_NAME=$3
+
+docker build -f ../docker/dockerfile-cli -t $REPO_NAME ../docker
+
+docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+$(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
+
+
+echo "step1"
+
+aws ecr describe-repositories --repository-names $REPO_NAME || aws ecr create-repository --repository-name $REPO_NAME
+
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
