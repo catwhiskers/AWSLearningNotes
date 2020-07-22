8787f39243095dd9e8bbaee34885b194e86f9528
aws_cam <catwhiskers@me>
2020-07-22 10:31:25 -0400

    add install

---
+++ dl-social-distancing-app/install.sh
@@ -0,0 +1,2 @@
+pip3.7 install scipy -t ./
+pip3.7 install imutils -t ./
--- dl_yolov5_onnx/install.sh
+++ dl_yolov5_onnx/install.sh
@@ -1,2 +1,4 @@
-sudo pip3 install onnx onnxruntime -t ./
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.5 get-pip.py 
+pip3 install onnx onnxruntime -t ./
 sudo apt-get install libatlas-base-dev
